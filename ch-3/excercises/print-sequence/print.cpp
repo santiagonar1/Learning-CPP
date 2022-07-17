41f498ab528b2b4a9eb2181bf6ef61be7bbf91a5
Santiago Narvaez <santiago.narvaez@tum.de>
2022-07-17 13:08:35 +0200

    Add printing ordered int. seq. ch3

---
+++ ch-3/excercises/print-sequence/print.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main(int, char **) {
+  int val1{0}, val2{0}, val3{0};
+  int first{0}, second{0}, third{0};
+
+  std::cout << "Give 3 integers in any order: \n";
+  std::cout << "> ";
+  std::cin >> val1 >> val2 >> val3;
+
+  if (val1 < val2) {
+    if (val1 < val3) {
+      first = val1;
+      if (val2 < val3) {
+        second = val2;
+        third = val3;
+      } else {
+        second = val3;
+        third = val2;
+      }
+    } else {
+      first = val3;
+      second = val1;
+      third = val2;
+    }
+  } else if (val2 < val3) {
+    first = val2;
+    if (val1 < val3) {
+      second = val1;
+      third = val3;
+    } else {
+      second = val3;
+      third = val1;
+    }
+  } else {
+    first = val3;
+    second = val2;
+    third = val1;
+  }
+
+  std::cout << first << ", " << second << ", " << third << "\n";
+}
