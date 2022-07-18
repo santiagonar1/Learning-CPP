800ec9c6fcde26edeb7a118324bdeec913db9d2b
Santiago Narvaez <santiago.narvaez@tum.de>
2022-07-18 16:54:24 +0200

    Include conversion from Yuan

--- ch-4/currency-converter/converter.cpp
+++ ch-4/currency-converter/converter.cpp
@@ -4,12 +4,13 @@ int main(int, char **) {
   constexpr double dollar_per_yen = 0.0072;
   constexpr double dollar_per_kroner = 0.099;
   constexpr double dollar_per_pound = 1.2;
+  constexpr double dollar_per_yuan = 0.15;
 
   char c{0};
   double money{0};
 
   std::cout << "Please indicate the amount followed by the currency: (y)en, "
-               "(k)roner or (p)ounds\n";
+               "(k)roner, (p)ounds, or y(u)an\n";
   std::cout << "> ";
   std::cin >> money >> c;
 
@@ -24,6 +25,9 @@ int main(int, char **) {
   case 'p':
     std::cout << money << " dollars =" << money * dollar_per_pound
               << " pounds\n";
+    break;
+  case 'u':
+    std::cout << money << " dollars =" << money * dollar_per_yuan << " yuan\n";
     break;
   default:
     std::cout << "I cannot recognize " << c << "\n";
