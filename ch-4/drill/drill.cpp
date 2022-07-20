68ef5b4234d836bb2818d2bcd0cf8bff01551154
Santiago Narvaez <santiago.narvaez@tum.de>
2022-07-20 08:34:41 +0200

    Print larger and smaller number

--- ch-4/drill/drill.cpp
+++ ch-4/drill/drill.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
-int main(int, char**) {
-    std::cout << "> ";
+int main(int, char **) {
+  std::cout << "> ";
 
-    int val1 = 0;
-    int val2 = 0;
-    while (std::cin >> val1 >> val2) {
-        std::cout << val1 << " " << val2 << '\n';
+  int val1 = 0;
+  int val2 = 0;
+  while (std::cin >> val1 >> val2) {
+    if (val1 < val2) {
+      std::cout << "Smaller is: " << val1 << "\tLarger is: " << val2 << '\n';
+    } else if (val2 < val1) {
+      std::cout << "Smaller is: " << val2 << "\tLarger is: " << val1 << '\n';
     }
+  }
 }
