9b3ab72b862d8da204d427601881737baddb5e96
Santiago Narvaez <santiago.narvaez@tum.de>
2022-07-20 09:11:23 +0200

    Read units

--- ch-4/drill/drill.cpp
+++ ch-4/drill/drill.cpp
@@ -1,14 +1,16 @@
 #include <cmath>
+#include <string>
 #include <iostream>
 #include <limits>
 
 int main(int, char **) {
   double val = 0;
+  std::string unit = "";
   double smallest = std::numeric_limits<double>::max();
   double largest = std::numeric_limits<double>::min();
 
   std::cout << "> ";
-  while (std::cin >> val) {
+  while (std::cin >> val >> unit) {
     if (val < smallest) {
         std::cout << "The smallest so far: " << val << '\n';
         smallest = val;
