58cc4b3517fed3fe0f2abcbfba51a6b8b75b1e8a
Santiago Narvaez <santiago.narvaez@tum.de>
2022-07-20 09:36:15 +0200

    Keep the sum

--- ch-4/drill/drill.cpp
+++ ch-4/drill/drill.cpp
@@ -12,21 +12,41 @@ int main(int, char **) {
   double smallest = std::numeric_limits<double>::max();
   double largest = std::numeric_limits<double>::min();
 
+  constexpr double cm_in_m = 100;
+  constexpr double cm_in_inch = 2.54;
+  constexpr double inch_in_ft = 12;
+  double sum = 0;
+
   std::cout << "> ";
   while (std::cin >> val >> unit) {
     if (std::find(valid_units.begin(), valid_units.end(), unit) !=
         valid_units.end()) {
+
+      if (unit == "cm") {
+        val /= cm_in_m;
+      } else if (unit == "in") {
+        val = (val * cm_in_inch) / cm_in_m;
+      } else if (unit == "ft") {
+        const double val_in_inch = val * inch_in_ft;
+        val = (val_in_inch * cm_in_inch) / cm_in_m;
+      }
+
+      sum += val;
+
       if (val < smallest) {
-        std::cout << "The smallest so far: " << val << '\n';
+        std::cout << "The smallest so far: " << val << "m\n";
         smallest = val;
       }
 
       if (largest < val) {
-        std::cout << "The largest so far: " << val << '\n';
+        std::cout << "The largest so far: " << val << "m\n";
         largest = val;
       }
+
     } else {
-        std::cout << unit << " not recognized\n";
+      std::cout << unit << " not recognized\n";
     }
   }
+
+  std::cout << "Total: " << sum << "m\n";
 }
