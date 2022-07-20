b7120f2cde3d9a8ffb85bc7f33836f9edb99b595
Santiago Narvaez <santiago.narvaez@tum.de>
2022-07-20 09:16:44 +0200

    Reject numbers without valid units

--- ch-4/drill/drill.cpp
+++ ch-4/drill/drill.cpp
@@ -1,24 +1,32 @@
+#include <algorithm>
 #include <cmath>
-#include <string>
 #include <iostream>
 #include <limits>
+#include <string>
+#include <vector>
 
 int main(int, char **) {
   double val = 0;
   std::string unit = "";
+  const std::vector<std::string> valid_units = {"cm", "m", "in", "ft"};
   double smallest = std::numeric_limits<double>::max();
   double largest = std::numeric_limits<double>::min();
 
   std::cout << "> ";
   while (std::cin >> val >> unit) {
-    if (val < smallest) {
+    if (std::find(valid_units.begin(), valid_units.end(), unit) !=
+        valid_units.end()) {
+      if (val < smallest) {
         std::cout << "The smallest so far: " << val << '\n';
         smallest = val;
-    }
-    
-    if (largest < val) {
+      }
+
+      if (largest < val) {
         std::cout << "The largest so far: " << val << '\n';
         largest = val;
+      }
+    } else {
+        std::cout << unit << " not recognized\n";
     }
   }
 }
