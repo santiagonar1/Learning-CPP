1fb27c0e538708f9a3771137f763675b1136c248
Santiago Narvaez <santiago.narvaez@tum.de>
2022-07-21 20:49:59 +0200

    Implement code with recursion

--- ch-4/excercises/guess-number/guess.cpp
+++ ch-4/excercises/guess-number/guess.cpp
@@ -1,58 +1,39 @@
 #include <iostream>
 
+void guess_number(const int lower_bound, const int upper_bound);
+
 int main(int, char **) {
+  constexpr int lower_boundary = 1;
   constexpr int upper_boundary = 100;
 
-  int lower_num = 1;
-  int upper_num = upper_boundary;
-  std::cout << "Think of a number between " << lower_num << " and " << upper_num
-            << '\n';
+  std::cout << "Think of a number between " << lower_boundary << " and "
+            << upper_boundary << '\n';
 
+  guess_number(lower_boundary, upper_boundary);
+}
+
+void guess_number(const int lower_bound, const int upper_bound) {
   char input = ' ';
-
-  while (upper_num - lower_num > 2) {
-    const int middle_point = (lower_num + upper_num) / 2;
-    char input = ' ';
+  if (lower_bound == upper_bound) {
+    std::cout << "Your number is " << lower_bound << '\n';
+  } else if (upper_bound - lower_bound == 1) {
+    std::cout << "Is your number greater than " << lower_bound << "?\n";
+    std::cout << "> ";
+    std::cin >> input;
+    if (input == 'y') {
+      std::cout << "Your number is " << upper_bound << "\n";
+    } else {
+      std::cout << "Your number is " << lower_bound << "\n";
+    }
+  } else {
+    const int middle_point = (lower_bound + upper_bound) / 2;
     std::cout << "Is your number greater than " << middle_point << "?\n";
     std::cout << "> ";
     std::cin >> input;
-
     if (input == 'y') {
-      lower_num = middle_point;
+      guess_number(middle_point + 1, upper_bound);
     } else {
-      upper_num = middle_point;
-    }
-  }
-
-  if (upper_num == lower_num) {
-    std::cout << "Your number is " << upper_num << "\n";
-  } else if (upper_num - lower_num == 1) {
-    char input = ' ';
-    std::cout << "Is your number greater than " << lower_num << "?\n";
-    std::cout << "> ";
-    std::cin >> input;
-    if (input == 'y') {
-      std::cout << "Your number is " << upper_num << "\n";
-    } else {
-      std::cout << "Your number is " << lower_num << "\n";
-    }
-  } else {
-    char input = ' ';
-    std::cout << "Is your number greater than " << (upper_num + lower_num) / 2
-              << "?\n";
-    std::cout << "> ";
-    std::cin >> input;
-    if (input == 'y') {
-      std::cout << "Your number is " << upper_num << "\n";
-    } else {
-      std::cout << "Is your number greater than " << lower_num << "?\n";
-      std::cout << "> ";
-      std::cin >> input;
-      if (input == 'y') {
-        std::cout << "Your number is " << upper_num << "\n";
-      } else {
-        std::cout << "Your number is " << lower_num << "\n";
-      }
+      guess_number(lower_bound, middle_point);
     }
   }
 }
