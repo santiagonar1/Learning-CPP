cf045e00eab7bb810d7069f2908f3d168755d47f
Santiago Narvaez <santiagonar1@gmail.com>
2022-08-03 08:57:25 +0200

    [CH5] Move error checking from main body to function

--- ch-5/exercises/buggy-temp-converter/main.cpp
+++ ch-5/exercises/buggy-temp-converter/main.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
 
+class TemperatureError {};
+
 double ctok(double c);
 
 int main(int, char **) {
   double c = 0;
-  constexpr double absolute_zero = -273.5;
   std::cout << "Introduce temp. in Celsius\n";
   std::cout << "> ";
   std::cin >> c;
-  if (c < absolute_zero) {
-    std::cout << "Error: Temperature below " << absolute_zero << "\n";
+  try {
+    double k = ctok(c);
+    std::cout << k << '\n';
+  } catch (TemperatureError) {
     return 1;
   }
-  double k = ctok(c);
-  std::cout << k << '\n';
 }
 
 double ctok(double c) {
   constexpr double c_to_k_ratio = 273.5;
+  constexpr double absolute_zero = -273.5;
+
+  if (c < absolute_zero) {
+    std::cout << "Error: Temperature below " << absolute_zero << "\n";
+    throw TemperatureError{};
+  }
+
   double k = c + c_to_k_ratio;
   return k;
 }
