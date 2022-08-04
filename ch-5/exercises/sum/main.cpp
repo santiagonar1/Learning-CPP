9e9f42ddd570ff5cf68556cc50deedf4147ed4c5
Santiago Narvaez <santiagonar1@gmail.com>
2022-08-04 09:04:49 +0200

    [CH5] Test for narrowing errors

--- ch-5/exercises/sum/main.cpp
+++ ch-5/exercises/sum/main.cpp
@@ -3,6 +3,10 @@
 #include <string>
 #include <vector>
 
+class NarrowError {};
+
+template <class R, class A> R narrow_cast(const A &a);
+
 int main(int, char **) {
   std::vector<int> values;
 
@@ -18,11 +22,15 @@ int main(int, char **) {
 
   while (input != "|") {
     try {
-      const int value = std::stoi(input);
+      const double d_value = std::stod(input);
+      const int value = narrow_cast<int>(d_value);
       values.push_back(value);
       std::cin >> input;
     } catch (std::invalid_argument) {
       std::cerr << "ERROR: " << input << " Not a number\n";
+      input = "|";
+    } catch (NarrowError) {
+      std::cerr << "ERROR: Narrowing a double\n";
       input = "|";
     }
   }
@@ -36,4 +44,12 @@ int main(int, char **) {
       std::accumulate(values.begin(), values.begin() + num_values, 0);
 
   std::cout << "Result: " << sum << '\n';
+}
+
+// run-time checked narrowing cast (type conversion).
+template <class R, class A> R narrow_cast(const A &a) {
+  R r = R(a);
+  if (A(r) != a)
+    throw NarrowError{};
+  return r;
 }
