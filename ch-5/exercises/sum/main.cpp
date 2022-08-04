3766b5eb2cce7343403c42f8bfea54d845d5c2ad
Santiago Narvaez <santiagonar1@gmail.com>
2022-08-04 09:16:34 +0200

    [CH5] Use doubles instead of ints

--- ch-5/exercises/sum/main.cpp
+++ ch-5/exercises/sum/main.cpp
@@ -8,7 +8,7 @@ class NarrowError {};
 template <class R, class A> R narrow_cast(const A &a);
 
 int main(int, char **) {
-  std::vector<int> values;
+  std::vector<double> values;
 
   int num_values = 0;
   std::cout << "Please indicate the number of values you want to sum:\n";
@@ -22,8 +22,7 @@ int main(int, char **) {
 
   while (input != "|") {
     try {
-      const double d_value = std::stod(input);
-      const int value = narrow_cast<int>(d_value);
+      const double value = std::stod(input);
       values.push_back(value);
       std::cin >> input;
     } catch (std::invalid_argument) {
@@ -40,8 +39,8 @@ int main(int, char **) {
     return 1;
   }
 
-  const int sum =
-      std::accumulate(values.begin(), values.begin() + num_values, 0);
+  const double sum =
+      std::accumulate(values.begin(), values.begin() + num_values, 0.0);
 
   std::cout << "Result: " << sum << '\n';
 }
